1238fb178f5e1bc93dde34dc31216373975b0ef7
Kira Cheng <kira@uber.com>
2018-04-16 00:45:17 +0800

    修改cn-generator.rb，新增对翻译文件数量和进度的统计。

--- _plugins/cn-generator.rb
+++ _plugins/cn-generator.rb
@@ -9,13 +9,19 @@ module Jekyll
         return
       end
       puts "Configuration file: 启用本地化文件，中文"
+      totalNum = site.pages.size
+      currentNum = 0
       site.pages.each do |page|
         newPath = "#{site.source}/#{PREFIX}/#{page.path}"
         isFileExist = File.exist?(newPath)
         if isFileExist
           site.pages.delete(page)
+          currentNum += 1
         end
       end
+      completeP = currentNum * 100 / totalNum.to_f
+      completePStr = format('%.2f', completeP)
+      puts "=================== 当前翻译进度：#{currentNum}/#{totalNum}，占比：#{completePStr}%"
       # site.pages.each do |page|
       #   puts "#{page.url} => #{page.path}"
       # end
